bcb300def77e5a883f6b37aaa59cc31661828f5c
carsonp45 <carsonp3@nycstudents.net>
2026-02-25 10:30:34 -0500

    Day 13

--- NumberG.py
+++ NumberG.py
@@ -1,12 +1,20 @@
 import random
 random_number = random.randint(1,10)
-Number = ""
-
-while Number != random_number:
-    try: 
-Number_input = input("Guess the correct number? (type random number from 1-10 to finish):")
-Number = int(guess_input)
-if Number == random_number:
-    print("Correct!")
-else:
-    print("Wrong! Try again.")
+guess = None
+guesses = []
+while guess != random_number:
+    Number_input = input("Guess the correct number? (type random number from 1-10 to finish):")
+    guess = int(Number_input)
+    guesses.append[guess]
+    guess += 1
+    if guess == random_number:
+        print("Correct!")
+        print("These were all the guesses you made", guesses)
+    else:
+        print("Wrong! Try again.")
+    if guess > random_number:
+        print ("Lower")
+        print("These were all the guesses you made", guesses)
+    if guess < random_number:
+        print ("Higher")
+        print("These were all the guesses you made", guesses)
